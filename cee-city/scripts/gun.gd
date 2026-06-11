d1baa9ab64d27708bee7ac5522d9eb0d03af14a4
rimam-byte <237639907+rimam-byte@users.noreply.github.com>
2026-06-12 09:36:12 +1200

    setting up bullet

--- cee-city/scripts/gun.gd
+++ cee-city/scripts/gun.gd
@@ -1,5 +1,12 @@
 extends Node2D
 @onready var marker_2d: Marker2D = $Marker2D
+const Bullet = preload('res://assets/bullet.png')
 
 func _process(delts:float) -> void:
 	look_at(get_global_mouse_position())
+
+func shoot() -> void:
+	var new_bullet = Bullet.instantiate()
+	new_bullet.position= marker_2d.global_position
+	new_bullet.target_position = (get_global_mouse_position()- marker_2d.global_position).normalized()
+	level.world.add_child()
