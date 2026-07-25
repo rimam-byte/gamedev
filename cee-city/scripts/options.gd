3381f4cb82008030ca662958be5bb4ad13efcde0
rimam-byte <237639907+rimam-byte@users.noreply.github.com>
2026-07-25 18:38:35 +1200

    fixing options

--- cee-city/scripts/options.gd
+++
@@ -1,3 +0,0 @@
-extends Control
-func _on_back_pressed() -> void:
-	get_tree().quit()
--- cee-city/scripts/pausemenu.gd
+++ cee-city/scripts/pausemenu.gd
@@ -7,6 +7,7 @@ func _on_restart_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file('res://scenes/level.tscn')
 func _on_options_pressed() -> void:
+	SceneManager.previous_scene='pause'
 	get_tree().paused = false
 	get_tree().change_scene_to_file('res://scenes/options.tscn')
 func _on_exit_pressed() -> void:
