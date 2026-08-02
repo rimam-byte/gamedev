c291d9e55462b527309e4866dcee5c9766e0c088
rimam-byte <rimam@students.mags.school.nz>
2026-08-02 18:29:41 +1200

    adding mines

--- cee-city/scripts/enemies.gd
+++ cee-city/scripts/enemies.gd
@@ -1,33 +1,22 @@
 extends CharacterBody2D
 @export var speed := 100.0
+@export var mine_scene: PackedScene
+@export var mine_chance= 0.3 #30 percent of drones throw mines
+
+var will_throw_mine = false
+var has_dropped = false
+
+func _ready():
+	will_throw_mine = randf()<mine_chance
 
 func _physics_process(delta: float) -> void:
 	position.x -= speed *delta
+	if will_throw_mine and not has_dropped and position.x<300:
+		has_dropped = true
+		drop_mine()
 
-
-#var direction:= Vector2.ZERO
-#var change_timer := 0.0
-
-
-
-#func _ready():
-	#pick_new_direction()
-	
-#func _physics_process(delta):
-	#change_timer -= delta
-	
-	#for changing directions
-	
-	##if change_timer <=0:
-		#pick_new_direction()
 		
-	#velocity = direction * speed
-	#move_and_slide()
-	
-#func pick_new_direction():
-	#direction = Vector2(
-		#randf_range (-1,1),
-		#randf_range (-1,1)
-	#).normalized()
-	
-	#change_timer = randf_range (1.0, 3.0)
+func drop_mine():
+	var mine = mine_scene.instantiate()
+	mine.global_position = global_position
+	get_tree().current_scene.add_child(mine)
