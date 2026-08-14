ad2a64b0e0c3881b1e757c03d7afd63cc252d2b0
rimam-byte <rimam@students.mags.school.nz>
2026-08-14 20:21:46 +1200

    testing tech 2 bird

--- cee-city/scripts/bird.gd
+++ cee-city/scripts/bird.gd
@@ -3,11 +3,11 @@ extends Area2D
 @export var speed := 100.0
 var direction:= Vector2.ZERO
 
+
 func _process(delta):
 	position += direction * speed * delta
 	if global_position.x < -700 or global_position.x >700 or global_position.y<-400 or global_position.y>400:
 		queue_free()
-
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group('bullet'):
